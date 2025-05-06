b5fde8c17fec38179ee52a581a202c7c1a3fade7
valec3 <v1c70rotakmer@gmail.com>
2025-05-06 17:35:52 -0500

    add auth

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,23 @@
-import { Button } from "@/components/ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignIn,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
+    <div className="flex flex-col items-center justify-center min-h-svh bg-slate-800 gap-y-5">
+      <header className="flex items-center justify-between w-full max-w-4xl p-4 bg-white rounded-lg shadow-md">
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+      <SignIn />
     </div>
   );
 }
