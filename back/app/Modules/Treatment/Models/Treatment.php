1d86190418c3dd6ad093b1ecddfd1b24c327bde3
cometa <renerolando321@egmail.com>
2025-06-16 16:00:08 -0500

    tratamientos

---
+++ back/app/Modules/Treatment/Models/Treatment.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Modules\Patient\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class
+{
+    // Nombre de la tabla (opcional si sigue la convención plural: patients)
+    protected $table = 'treatment';
+
+    // Clave primaria (opcional si es "id")
+    protected $primaryKey = 'id';
+
+    // Si no usas timestamps (created_at, updated_at)
+    public $timestamps = false;
+
+    // Campos que pueden ser asignados masivamente
+    protected $fillable = [
+        'name',
+        'description',
+        'cost',
+    ];
+}
